8cd84f55a39d80cbd298df8901fadae304ab03b2
shravan097 <shravan097@users.noreply.github.com>
2022-03-23 11:46:19 -0700

    Remove Debug logs

--- src/main.ts
+++ src/main.ts
@@ -15,12 +15,12 @@ async function run(): Promise<void> {
     core.info('Parsing done')
     const coverageByFile = filterCoverageByFile(parsedCov)
     core.info('Filter done')
-    core.info(JSON.stringify(coverageByFile, null, 2))
     const res = await annotateGithub(coverageByFile, GITHUB_TOKEN)
     core.info('Annotation done')
     core.info(JSON.stringify(res, null, 2))
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
+    core.info(JSON.stringify(error))
   }
 }
 
