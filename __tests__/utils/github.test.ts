c70cd37993c04f5bf2698e4791c3300e39f8efbc
shravan097 <shravan097@users.noreply.github.com>
2022-03-31 09:50:27 -0700

    add test

--- __tests__/utils/github.test.ts
+++ __tests__/utils/github.test.ts
@@ -1,4 +1,7 @@
 import {test} from '@jest/globals'
+import {Github} from '../../src/utils/github'
 test('filterCoverageByFile', function () {
+
+  const github = new Github('12345')
   console.log('hi')
 })
